f265037a82abef21e815ff5f3455d0adb63847d5
Purnima Kesarwani <purnimakesarwani47@gmail.com>
2024-09-01 01:54:56 +0530

    Basic of C++

---
+++ basics.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // for next line use (endl or "\n") otherwise it will print in one line
+    // for printing
+    cout << "Hii ";
+    cout << "I am CPP" << endl;
+    cout << "Basic Programming Language" << endl;
+
+    int x;  // declaration
+    x = 10; // initialization
+
+    int y = 50, z = 32;
+    cout << y + z;
+
+    // increment - decrement
+    // x++,++x,y--,--y
+
+    // taking input
+    int inp;
+    cin >> inp;
+    cout << inp + inp;
+
+    // ascii values
+    // A - Z : 65 TO 90
+    // a - z : 97 to 122
+    // 0 - 9 : 48 to 57
+
+    // CONDITIONALS 
+    // if-else, nested, else if ladder, ternary, switch
+
+    // we can use && (and), || (or).
+
+
+    // LOOPS, FUNCTIONS --> same as java
+
+}
